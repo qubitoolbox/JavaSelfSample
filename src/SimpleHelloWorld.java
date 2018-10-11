8503744d9e88c4bcd87d63fe679e8bf6226d7b8c
qubitAI <osmanmorales@ymail.com>
2018-10-11 03:15:13 -0400

    Update SimpleHelloWorld.java

--- src/SimpleHelloWorld.java
+++ src/SimpleHelloWorld.java
@@ -7,16 +7,9 @@
 
 import javax.swing.JOptionPane;
 public class SimpleHelloWorld {
-    
-    
-    public static void main(String[] args){
-    
+    public static void main(String[] args)
+    {
         JOptionPane.showMessageDialog(null,"This is the most basic JAVA output"
                 + "", "HI!", JOptionPane.INFORMATION_MESSAGE);
-    
-    
     }
-    
-    
-    
 }
