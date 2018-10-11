f05c2d0614434032bcb8177fd8b4376ab84708fd
qubitAI <osmanmorales@ymail.com>
2018-10-11 03:13:23 -0400

    Update GetData.java

--- src/GetData.java
+++ src/GetData.java
@@ -1,22 +1,20 @@
 //Generic class for retrieving user values
 
 import javax.swing.JOptionPane;
-
-public class GetData {
-    
-    public static double getDouble(String s){
-        
+public class GetData 
+{
+    public static double getDouble(String s)
+    {
         return Double.parseDouble(getString(s));
     }
     
-    public static int getInt(String s){
-        
+    public static int getInt(String s)
+    {
         return Integer.parseInt(getString(s));
     }
-    
-    public static String getString(String s){
+    public static String getString(String s)
+    {
         
         return JOptionPane.showInputDialog(s);
     }
-    
 }
