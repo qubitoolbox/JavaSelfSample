91287d070a2620ffe6cc9135abc67fdf8139d764
qubitAI <osmanmorales@ymail.com>
2018-10-11 03:12:27 -0400

    Update Methods.java

--- src/Methods.java
+++ src/Methods.java
@@ -6,28 +6,20 @@
  */
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
-public class Methods {
+public class Methods 
+{
     
     
     // create 1D array for testing
     String[] strArr = {"Testing", "Testing2", "Testing3"};
-    
-    String getStringArray(){
-       
+    String getStringArray()
+    {
         return strArr.toString(); // of course this doesnt work. Requires looping
-    
     }
-    
-    public static void main(String[] args){
-    
+    public static void main(String[] args)
+    {
         Methods m = new Methods();
         JOptionPane.showMessageDialog(null, m.getStringArray(), "", JOptionPane.INFORMATION_MESSAGE);
-        
-    
     }
-    
-    
-    
-    
     
 }
